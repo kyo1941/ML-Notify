dea66b32161283f0c5d4ee7f55d9f3d4a01c50a4
kyo1941 <kyo1941@outlook.jp>
2025-05-24 03:24:34 +0900

    feat: Hiltモジュールの作成

---
+++ app/src/main/java/com/example/ml_notify/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.ml_notify.di
+
+import com.example.ml_notify.data.TaskDataHandlerImpl
+import com.example.ml_notify.domain.TaskDataHandler
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+
+    // TaskDataHandler型にはTaskDataHandlerImplのインスタンスを注入する
+    @Binds
+    @Singleton
+    abstract fun bindTaskDataHandler(taskDataHandlerImpl: TaskDataHandlerImpl): TaskDataHandler
+}
