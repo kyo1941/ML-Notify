3fa07dcd4320ab443bd58bc261c980143a47e7c3
kyo1941 <kyo1941@outlook.jp>
2025-05-31 04:31:42 +0900

    fix: スキーマはエクスポートしておく

--- app/src/main/java/com/example/ml_notify/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/ml_notify/data/db/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.ml_notify.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [TaskEntity::class], version = 1, exportSchema = false)
+@Database(entities = [TaskEntity::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
