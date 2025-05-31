1d999856928a84c61e0d585fc3ab36c9610ca9a1
kyo1941 <kyo1941@outlook.jp>
2025-06-01 01:42:20 +0900

    feat: TypeConverterでEnumを管理する

---
+++ app/src/main/java/com/example/ml_notify/data/db/Converters.kt
@@ -0,0 +1,18 @@
+package com.example.ml_notify.data.db
+
+import com.example.ml_notify.model.TaskStatus
+import androidx.room.TypeConverter
+
+class Converters {
+
+    // ordinate(Int)ではなくname(String)で保存されるようにする
+    @TypeConverter
+    fun fromTaskStatus(value: TaskStatus?): String? {
+        return value?.name
+    }
+
+    @TypeConverter
+    fun toTaskStatus(value: String?): TaskStatus? {
+        return value?.let { TaskStatus.valueOf(it) }
+    }
+}
