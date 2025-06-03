00e5db9c8225b75a2f056acd4b91bedf19567101
kyo1941 <kyo1941@outlook.jp>
2025-06-04 05:10:19 +0900

    feat: 遷移処理

--- app/src/main/java/com/example/ml_notify/MainActivity.kt
+++ app/src/main/java/com/example/ml_notify/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.ml_notify.ui.main.MainScreen
+import com.example.ml_notify.ui.main.TaskDetailScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState:   Bundle?) {
@@ -15,7 +19,11 @@ class MainActivity : ComponentActivity() {
             Surface(
                 modifier = Modifier.fillMaxSize()
             ) {
-                MainScreen()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "main") {
+                    composable("main") { MainScreen(navController = navController) }
+                    composable("taskDetail") { TaskDetailScreen() }
+                }
             }
         }
     }
--- app/src/main/java/com/example/ml_notify/ui/main/TaskDetailScreen.kt
+++ app/src/main/java/com/example/ml_notify/ui/main/TaskDetailScreen.kt
@@ -1,2 +1,11 @@
 package com.example.ml_notify.ui.main
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskDetailScreen (
+
+) {
+    Text("hogehoge")
+}
